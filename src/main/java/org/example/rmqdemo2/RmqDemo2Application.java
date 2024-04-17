3dceb49cb7315c28d5c81d95466a91481ffc69c7
Ilya_Spasskiy <spasskiy.iv@yandex.ru>
2024-04-17 13:43:59 +0700

    first commit

---
+++ src/main/java/org/example/rmqdemo2/RmqDemo2Application.java
@@ -0,0 +1,13 @@
+package org.example.rmqdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmqDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmqDemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rmqdemo2/RmqDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rmqdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmqDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
